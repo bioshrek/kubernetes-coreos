bfd2326045fb3570749ec15c7c13f1e45fb86ee0
bioshrek <shrekwang1@gmail.com>
2014-10-11 15:21:57 +0000

    =add local build file

---
+++ build.sh
@@ -0,0 +1,2 @@
+mkdir -p /home/chianyu/kubernetes-coreos
+sudo docker run -v /home/chianyu/kubernetes-coreos:/home/chianyu/kubernetes-coreos bioshrek/kubernetes-coreos cp -rv /kubernetes-binaries /home/chianyu/kubernetes-coreos/
